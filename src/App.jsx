ef996b8460c04bedc65564f2cf9b1fcb9ad61799
Kuldeep <codysharma01@gmail.com>
2023-03-09 01:51:31 +0530

    Created Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,38 @@ import NavBar from "./NavBar";
 import ProjectsSection from "./ProjectsSection";
 import TechStackSection from "./TechStack";
 import AboutMeSection from "./About";
+import CTASection from "./CTA";
 import FooterSection from "./Footer";
 
 function App() {
   return (
-    <div className="font-poppins md:max-w-fourk md:mx-auto">
-      {/* centerised container  */}
-      <div className="mx-5 md:mx-10 lg:mx-20">
-        <div id="home" className="mb-16">
-          <NavBar />
-          <HeroSection />
-        </div>
+    <div className="font-poppins">
+      <div className="md:max-w-fourk md:mx-auto">
+        {/* centerised container  */}
+        <div className="mx-5 md:mx-10 lg:mx-20">
+          <div id="home" className="mb-16">
+            <NavBar />
+            <HeroSection />
+          </div>
 
-        <div id="projects" className="mb-24">
-          <ProjectsSection />
-        </div>
+          <div id="projects" className="mb-24">
+            <ProjectsSection />
+          </div>
 
-        <div id="tech-stack" className="mb-24">
-          <TechStackSection />
-        </div>
+          <div id="tech-stack" className="mb-24">
+            <TechStackSection />
+          </div>
 
-        <div id="about-me" className="mb-24">
-          <AboutMeSection />
-        </div>
+          <div id="about-me" className="mb-24">
+            <AboutMeSection />
+          </div>
 
-        <div id="cta" className="mb-24">
-          <FooterSection />
+          <div id="cta" className="mb-40">
+            <CTASection />
+          </div>
         </div>
       </div>
+      <FooterSection />
     </div>
   );
 }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,56 +1,18 @@
 import React from "react";
-import { SectionHeading } from "./components/SectionHeading";
-
-const CTAButton = ({ text, logo, link, className }) => {
-  return (
-    <a href={link} className={"flex justify-center" + " " + className}>
-      <div
-        className="flex gap-7 items-center justify-center bg-forgroundColor rounded-base
-      w-60 h-16  md:w-72 md:h-20 visited:bg-hoverForegroundColor hover:bg-hoverForegroundColor active:bg-hoverForegroundColor"
-      >
-        <span className="text-lg sm:text-base md:text-2xl text-white">
-          {text}
-        </span>
-        <img
-          src={logo}
-          alt="Social Media Logo"
-          className="inline-block w-8 h-8"
-        />
-      </div>
-    </a>
-  );
-};
 
 const Footer = () => {
-  const onCTAClickHandler = () => {};
   return (
     <div>
-      <div className="mb-16 text-center">
-        <SectionHeading
-          headingText="That's it"
-          subHeadingText="Now its your turn to say Hi"
-        />
-      </div>
+      <div className="bg-forgroundColor rounded-t-base">
+        <div className="flex flex-col px-8 py-6 sm:flex-row sm:justify-between md:max-w-fourk md:mx-auto">
+          <p className="text-white mb-5 sm:mb-0">Kuldeep Sharma © / 2023</p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-5">
-        <CTAButton
-          text="Linked In"
-          logo="./src/assets/logos/linkedin.svg"
-          link="https://linkedin.com/in/c0dysharma"
-        />
-
-        <CTAButton
-          text="Github"
-          logo="./src/assets/logos/github.svg"
-          link="https://github.com/c0dysharma"
-        />
-
-        <CTAButton
-          text="Email Me"
-          logo="./src/assets/logos/email.svg"
-          link="mailto:codysharma01@gmail.com"
-          className=""
-        />
+          <ul className="flex flex-col gap-1 sm:flex-row sm:gap-6 md:gap-24 font-medium text-mutedColor">
+            <a href="#projects">Projects</a>
+            <a href="#skills">Skills</a>
+            <a href="#about">About Me</a>
+          </ul>
+        </div>
       </div>
     </div>
   );
