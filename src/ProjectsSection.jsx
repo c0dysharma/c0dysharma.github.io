cf6cb621c4e470d901d35f57f5e900ac5d7894e0
Kuldeep <codysharma01@gmail.com>
2023-03-09 18:25:43 +0530

    Added project pics

--- src/ProjectsSection.jsx
+++ src/ProjectsSection.jsx
@@ -5,11 +5,10 @@ import { MobileCTABtn } from "./components/MobileCTABtn";
 
 const ProjectElement = ({ image, heading, subHeading, text, className }) => (
   <div className={className}>
-    <img
-      src={image}
-      alt="Project Preview Image"
-      className="w-full rounded-base mb-7"
-    />
+    <div
+      className="w-[400px] h-[300px] rounded-base mb-7 bg-red-100 bg-cover shadow-lg"
+      style={{ backgroundImage: image }}
+    ></div>
     <p className="text-forgroundColor font-extrabold">{heading}</p>
     <p className="text-mutedColor mb-3 text-sm">{subHeading}</p>
     <p className="text-forgroundColor text-sm sm:text-base">{text}</p>
@@ -35,28 +34,28 @@ const PorjectsSection = () => {
 
       <div className="grid gap-y-14 sm:gap-x-8 sm:grid-cols-2 lg:grid-cols-3">
         <ProjectElement
-          image="/assets/images/project-placeholder.jpg"
+          image="url('/assets/images/Dalle-img.png')"
           heading="Dall-E 2.0 Website"
           subHeading="MongoDB, Expres JS, React JS, Node js"
           text="Experience the magic of unique and stunning images with my latest MERN stack project, which leverages the incredible Dall-E API to generate and display beautiful images in gallery."
         />
 
         <ProjectElement
-          image="/assets/images/project-placeholder.jpg"
+          image="url('/assets/images/Nike-img.png')"
           heading="Nike E-commerce App"
           subHeading="Node Js React Native, Redux"
           text="Shop for the latest Nike products with ease using our innovative ecommerce app. Built with React Native, Zustand state management, this app delivers the ultimate shopping experience."
         />
 
         <ProjectElement
-          image="/assets/images/project-placeholder.jpg"
+          image="url('/assets/images/Social-img.png')"
           heading="Social Media Backend"
           subHeading="MongoDB, Expres JS, Node js"
           text="This social media backend app is built with Node.js, Express.js, and MongoDB, and includes features like post liking, commenting, and following users."
         />
 
         <ProjectElement
-          image="/assets/images/project-placeholder.jpg"
+          image="url('/assets/images/Greets-img.png')"
           heading="Greets Webapp"
           subHeading="MongoDB, Expres JS, Node js"
           text="Introducing Greets, a backend application designed to help you schedule emails, WhatsApp messages, and text SMS ahead of time for special occasions to make em feel special."
@@ -64,7 +63,7 @@ const PorjectsSection = () => {
         />
 
         <ProjectElement
-          image="/assets/images/project-placeholder.jpg"
+          image="url('/assets/images/Looter-img.png')"
           heading="Looter’s Backend"
           subHeading="Node Js React Native, Redux"
           text="The backend of the Looter app provides users with access to exclusive deals on popular ecommerce platforms, along with notification support and a variety of amazing offers."
@@ -72,7 +71,7 @@ const PorjectsSection = () => {
         />
 
         <ProjectElement
-          image="/assets/images/project-placeholder.jpg"
+          image="url('/assets/images/Scrappers-img.png')"
           heading="Web Scrappers"
           subHeading="MongoDB, Expres JS, Node js"
           text="My recent projects involved web scraping, data mining, and reverse engineering APIs from popular websites like Netflix, Amazon, and Oyo to extract valuable insights"
